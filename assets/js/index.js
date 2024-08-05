e63ad93f3a850ea06278da71daf4bc24d9a41242
The-King004 <ahmedadama231@gmail.com>
2024-08-05 14:17:21 +0100

    navbar responsive

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+
+// selection des elements dans le fichier html
+const NavLinks = document.querySelector(".navLinks")
+const ButtonMenu = document.getElementById('menu')
+const ButtonClose = document.getElementById('close')
+
+
+ButtonMenu.addEventListener('click', () => {
+    NavLinks.classList.add("mobile")
+})
+
+ButtonClose.addEventListener('click', ()=> {
+    NavLinks.classList.remove("mobile")
+})
